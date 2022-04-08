2f6f5fc48a33c4fde9d7c1a3dec31c8ba8cec055
Asepsrn17 <asepsrn17@gmail.com>
2022-04-08 22:11:30 +0700

    membuat komponen sederhana

---
+++ src/Materi/Komponen/pembahasan/FunctionalComponent.jsx
@@ -0,0 +1,9 @@
+const FunctionalComponent = ({ nama }) => {
+    return (
+        <div>
+            <h3>membuat komponent dengan functional component</h3>
+            <h3>hallo {nama}, selamat belajar</h3>
+        </div>
+    )
+}
+export default FunctionalComponent;
